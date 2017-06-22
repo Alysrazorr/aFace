8858483e128029e0b0e0592891fab524cb1fb5b0
alysrazorr <alysrazorr@gmail.com>
2017-06-22 23:03:33 +0800

    1

--- js/aface.base.js
+++ js/aface.base.js
@@ -6,17 +6,6 @@ Clazz.create = function(ms) {
 	    };
 		this['init'] = !this['init'] ? undefined : this['init'];
 		this['uuid'] = !this['uuid'] ? undefined : this['uuid'];
-        this['getLoader'] = function() {
-            if (!top.window.page.loader) {
-                $(top.window.document.body).loader();
-                top.window.page.loader = $(top.window.document.body).target('loader');
-            }
-            return top.window.page.loader;
-        };
-        this['hasUrlPermission'] = function(url) {
-            var menusMap = top.window.page.menusMap;
-            return this.isGod ? true : typeof menusMap[url] === 'undefined' ? false : menusMap[url];
-        };
 	};
 	return o;
 };
@@ -27,29 +16,6 @@ UUID.generate = function() {
         var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
         return v.toString(16);
     }).toUpperCase();
-};
-
-var StringUtils = window.StringUtils || {};
-StringUtils.trim = function(string){
-    return string.replace(/(^\s*)|(\s*$)/g, "");
-};
-StringUtils.isNotBlank = function(string) {
-    if (string) {
-        if ('' === StringUtils.trim(string + '')) {
-            return false;
-        } else {
-            return true;
-        }
-    } else {
-        return false;
-    }
-};
-StringUtils.isNotNull = function(string) {
-    if ("null" == string) {
-        return false;
-    } else {
-        return StringUtils.isNotBlank(string);
-    }
 };
 
 (function($) {
@@ -71,14 +37,3 @@ StringUtils.isNotNull = function(string) {
         return o;
     };
 })(jQuery);
-
-function clearForm(form) {
-    $(form).find('input:not([type="radio"])').val('');
-    $(form).find('textarea').html('');
-    $(form).find('input[type="radio"]').each(function(idx, elem) {
-        if ($(this).attr('checked')) { $(this).click(); }
-    });
-    $(form).find('select').each(function(idx, elem) {
-        this.selectedIndex = 0;
-    });
-};
